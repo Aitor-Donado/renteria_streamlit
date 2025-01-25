3c3f32c5e2f472918f2b63dceb6aefb099059f21
Aitor Donado <aitor.donado@gmail.com>
2025-01-25 12:05:46 +0100

    Gráfico interactivo en Ejemplo_uso_api

--- Ejemplo_uso_api/uso_api_yf.py
+++ Ejemplo_uso_api/uso_api_yf.py
@@ -9,17 +9,25 @@ df = pd.read_csv("Empresas.csv")
 df = df.drop("Unnamed: 0", axis=1)
 st.dataframe(df)
 
-# Es un selector desplegable que sólo permite una elección
-opciones = df["empresa"]
-select = st.selectbox("Elige una empresa", options= opciones)
+col1, col2 = st.columns(2)
+
+with col1:
+    # Es un selector desplegable que sólo permite una elección
+    mercados = df["mercado"].unique()
+    mercado_elegido = st.selectbox("Elige un mercado", options= mercados)
+
+with col2:
+    # Es un selector desplegable que sólo permite una elección
+    if mercado_elegido:
+        df = df[df["mercado"]==mercado_elegido]
+    opciones = df["empresa"]
+    select = st.selectbox("Elige una empresa", options= opciones)
 if select:
     ticker = df[df["empresa"]==select]["ticker.yf"].values[0]
     st.write(ticker)
     empresa = yf.Ticker(ticker)
     hist_empresa = empresa.history(period="max")
     #st.table(hist_empresa)
-
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 
     fig = go.Figure(data=[go.Candlestick(x=hist_empresa.index,
                     open=hist_empresa['Open'],
